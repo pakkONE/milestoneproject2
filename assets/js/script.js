f502700ea79f71f2f6e5d7ef8933ba8290335342
pakkONE <johan.pakarinen@gmail.com>
2022-04-21 14:38:46 +0000

    Add initial functions to script file

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+// Defining all functions that are needed for the game
+
+function setGoal() {
+
+}
+
+function runTimer() {
+
+}
+
+function stopTimer() {
+
+}
+
+function checkScore() {
+
+}
+
+function addScore() {
+
+}
+
+function resetGame() {
+
+}
