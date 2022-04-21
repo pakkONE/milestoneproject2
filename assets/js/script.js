16572d6f225101d0cb908aeb0dd13d5f8d939292
pakkONE <johan.pakarinen@gmail.com>
2022-04-21 16:25:13 +0000

    Add some code to functions spaceBarUp and spaceBarDown

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,27 @@
+// event listeners
+
+document.addEventListener('keydown',
+    spaceBarDown);
+
+document.addEventListener('keyup', spaceBarUp);
+
 // Defining all functions that are needed for the game
 
-function setGoal() {
 
+
+function setGoal() {
+    
+}
+
+function spaceBarDown(event) {
+    if (event.code === 'Space') {
+        console.log(event)
+        console.log('spacebar is down!')
+    }
+}
+
+function spaceBarUp() {
+    console.log('spacebar is up!')
 }
 
 function runTimer() {
