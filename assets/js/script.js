45eb14f7f95fe38fd0ae4a1c3ec045ced1854c42
pakkONE <johan.pakarinen@gmail.com>
2022-05-19 14:13:04 +0000

    Add comments to all functions explaining what they are for

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,12 @@ let spaceBarIsDown = false;
 
 let scoreboard = [];
 
+
 // event listeners
 
 window.onload = (event) => {
     setGoal();
+    newGame();
 };
 
 
@@ -21,19 +23,22 @@ document.addEventListener('keydown',
 
 document.addEventListener('keyup', spaceBarUp);
 
+
 // Functions
 
+// A function to set the timegoal for the user
 function setGoal() {
     goal = getNewGoal();
     let timeGoal = document.getElementById("timegoal");
     timeGoal.innerHTML = goal;
 }
 
-
+// This returns a randomly generated number with three decimals
 function getNewGoal() {
     return Math.floor((Math.random() * (12 - 4) + 4) * 1000) / 1000;
 }
 
+// This function triggers the timecounter to start when the space bar key is pressed down
 function spaceBarDown(event) {
     if (event.code === 'Space' && !spaceBarIsDown) {
         spaceBarIsDown = true;
@@ -41,6 +46,7 @@ function spaceBarDown(event) {
     }
 }
 
+// This function ends the round and stops the timecounter when space bar key is released
 function spaceBarUp(event) {
     if (event.code === 'Space') {
         spaceBarIsDown = false;
@@ -49,6 +55,7 @@ function spaceBarUp(event) {
     event.preventDefault(); // Do not trigger new game when "New Game" button is in focus and space key down
 }
 
+// This function calculates time elapsed since space key was pressed down
 function startRound() {
     const d = new Date();
     let start = d.getTime();
@@ -66,10 +73,13 @@ function startRound() {
     }
 }
 
+// This function calculates the difference between the time goal 
+// and the elapsed time and returns it with three decimals
 function getScore() {
     return Math.abs(elapsedTime - goal).toFixed(3);
 }
 
+// This function returns a message to the user depending on the result of the score
 function endRound() {
     const resultspan = document.getElementById("resultplaceholder");
     if (roundIsWon()) {
@@ -90,10 +100,13 @@ function endRound() {
     }
 }
 
+// This function checks if the difference between elapsed time is within 0.2 second margin
 function roundIsWon() {
     return elapsedTime - goal <= 0.2 && elapsedTime - goal >= -0.2;
 }
 
+// This function resets the HTML to original text and color after 
+// game is finished when five rounds have been played 
 function resetRound() {
     const resultspan = document.getElementById("resultplaceholder");
     const counter = document.getElementById("timecounter");
@@ -103,12 +116,14 @@ function resetRound() {
     setGoal();
 }
 
+// This function populates the list items with color depending on result
 function setLi(round, color, score) {
     const li = document.getElementById(`round${round}`);
     li.style.backgroundColor = color;
     li.innerHTML = score;
 }
 
+// This function resets the game once the "New Game" button is clicked
 function newGame() {
     scoreboard = [];
     for (let i = 1; i <= 5; i++) {
